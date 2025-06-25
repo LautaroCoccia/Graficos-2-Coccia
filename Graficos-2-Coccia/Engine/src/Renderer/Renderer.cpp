6f91bab3d44835692f7a4d3e1565df0f41f2a6c2
LautaroCoccia <lautaroauditore@gmail.com>
2025-06-25 06:06:41 -0300

    Test

--- Graficos-2-Coccia/Engine/src/Renderer/Renderer.cpp
+++ Graficos-2-Coccia/Engine/src/Renderer/Renderer.cpp
@@ -147,17 +147,6 @@ namespace Engine
 		glDeleteBuffers(1, &ebo);
 	}
 
-	void Renderer::BindLight()
-	{
-		glGenVertexArrays(1, &lightVAO);
-		glBindVertexArray(lightVAO);
-		// we only need to bind to the VBO, the container's VBO's data already contains the data.
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		// set the vertex attribute 
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-		glEnableVertexAttribArray(0);
-	}
-
 	// ----------------------------
 
 	unsigned int Renderer::GetShader() 
