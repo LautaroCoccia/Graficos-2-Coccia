d6fe9bc4cae1f5d42b9e154babbe1ea9ba5e5844
LautaroCoccia <lautaroauditore@gmail.com>
2025-06-25 07:28:43 -0300

    Add spriteCube

---
+++ Graficos-2-Coccia/Engine/src/Cube/Cube.cpp
@@ -0,0 +1,53 @@
+#include "Cube.h"
+#include <iostream>
+namespace Engine
+{
+	Cube::Cube(const char* name, Renderer* renderer) : Entity()
+	{
+		for (int i = 0; i < 6; i++)
+		{
+			cube[i] = new Sprite(renderer);
+			cube[i]->InitTexture();
+			cube[i]->ImportTexture(name);
+			cube[i]->SetPosition(facesPositions[i]);
+			cube[i]->SetRotation(facesRotations[i]);
+		}
+	}
+	Cube::~Cube()
+	{
+		for (int i = 5; i >= 6; i--)
+		{
+			if (cube[i] != NULL)
+				delete cube[i];
+		}
+	}
+	void Cube::UpdatePosition()
+	{
+		for (int i = 0; i < 6; i++)
+		{
+			cube[i]->SetPosition(facesPositions[i] + _transform.position);
+		}
+	}
+	void Cube::Draw()
+	{
+		for (int i = 0; i < 6; i++)
+		{
+			cube[i]->Draw();
+		}
+	}
+	void Cube::TriggerCollision(Entity* other)
+	{
+		if (!other->GetStaticState())
+		{
+			glm::vec3 newPos = _transform.position;
+			ReturnToPrevPosition();
+			newPos -= _transform.position;
+
+			other->SetPosition(other->_transform.position + newPos);
+		}
+		else
+		{
+			ReturnToPrevPosition();
+		}
+	}
+}
---
+++ Graficos-2-Coccia/Engine/src/Cube/Cube.h
@@ -0,0 +1,39 @@
+#ifndef CUBE_H
+#define CUBE_H
+
+#include "..\Entity\Entity.h"
+#include "..\Sprite\Sprite.h"
+
+namespace Engine
+{
+
+	class EXPORT_API Cube : public Entity
+	{
+		Sprite* cube[6];
+		vec3 facesPositions[6] = {
+			vec3(0.0f, 0.0f, 0.5f),
+			vec3(0.0f, 0.0f, -0.5f),
+			vec3(0.5f, 0.0f, 0.0f),
+			vec3(-0.5f, 0.0f, 0.0f),
+			vec3(0.0f, 0.5f, 0.0f),
+			vec3(0.0f, -0.5f, 0.0f)
+		};
+		vec3 facesRotations[6] = {
+			vec3(0.0f, 0.0f, 0.0f),
+			vec3(0.0f, 0.0f, 0.0f),
+			vec3(0.0f, 90.0f, 0.0f),
+			vec3(0.0f, -90.0f, 0.0f),
+			vec3(90.0f, 0.0f, 0.0f),
+			vec3(-90.0f, 0.0f, 0.0f)
+		};
+	public:
+        Cube(const char* texture, Renderer* renderer);
+        ~Cube();
+		void UpdatePosition();
+		void Draw();
+		void TriggerCollision(Entity* other) override;
+	};
+}	
+#endif // !CUBE_H
+
+
